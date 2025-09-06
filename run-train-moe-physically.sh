0472498932bd3f4a859a394a5fbaf1ef5c94fb05
heiroz <1849197801@qq.com>
2025-09-07 02:01:55 +0800

    update

--- run-train-moe-physically.sh
+++ run-train-moe-physically.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-python -u train-moe_physically_scale.py \
+python -u train-moe_physically.py \
   --gpu 0 \
-  --ks 64\
+  --ks 128\
   --num_experts 64 \
   --es 1 2 4 8 16 \
   --heavisides False
